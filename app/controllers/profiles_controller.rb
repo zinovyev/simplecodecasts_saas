f148b7fe4a9f50c3fa1fcfd1ca820629224c5b83
Ivan Zinovyev <zinovyev.id@gmail.com>
2016-10-11 14:29:34 +0000

    Profiles#new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,8 @@
 class ProfilesController < ApplicationController
-	
+	def new
+		# form where each user can fill out their own profile
+		raise 'Access Denied' unless current_user.id == params[:user_id].to_i
+			@user = User.find(params[:user_id])
+			@profile = @user.build_profile
+	end
 end
