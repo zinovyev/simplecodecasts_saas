bdf59477dd22ddc6d8e760859534e39af4c6171c
Ivan Zinovyev <zinovyev.id@gmail.com>
2016-10-12 03:32:17 +0000

    Fix for profile-user link

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,7 @@ class ProfilesController < ApplicationController
 		# form where each user can fill out their own profile
 		raise 'Access Denied' unless current_user.id == params[:user_id].to_i
 			@user = User.find(params[:user_id])
-			@profile = @user.build_profile
+			@profile = Profile.new #@user.build_profile
 	end
 	
 	def create
