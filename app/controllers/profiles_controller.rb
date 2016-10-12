828bf062780d50b5bd42105ea0fb3a6194c3686d
Ivan Zinovyev <zinovyev.id@gmail.com>
2016-10-12 02:50:17 +0000

    User show action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -19,6 +19,6 @@ class ProfilesController < ApplicationController
 	
 	private
 		def profile_params
-			params.require(:params).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+			params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
 		end
 end
